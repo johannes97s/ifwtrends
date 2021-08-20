acf246c3b8f02d94d5f56f3499bc009bcfe7a970
johannes97s <johannes.schnebel@web.de>
2021-08-20 16:27:14 +0200

    Dateien erstellt

---
+++ R/Estimation.R
@@ -0,0 +1,30 @@
+setwd("~/Google Trends")
+library(tidyverse)
+library(gtrendsR)
+library(trendecon)
+library(tsbox)
+library(openxlsx)
+library(corrr)
+
+source("Trends.R")
+
+
+
+
+
+x1 <- lag(dat_comp$Index)
+y1 <- dat_comp$VDAXm
+
+fit1 <- lm(y1 ~ x1)
+summary(fit1)
+
+q <- quantile(x1, 0.75, na.rm = T)
+y2 <- y1[x1 >= q]
+x2 <- x1[x1 >= q]
+
+
+plot(x2, y2)
+fit2 <- lm(y2 ~ x2)
+summary(fit2)
+
+correlate(x2, y2)
---
+++ R/Google_Kategorien.R
@@ -0,0 +1,48 @@
+setwd("~/Google Trends")
+library(tidyverse)
+library(gtrendsR)
+library(trendecon)
+library(openxlsx)
+
+cat_paper <- as_tibble(read.csv("Kategorien_Woloszko.csv", head = F))
+names(cat_paper) = "name"
+cat<- as_tibble(categories)
+
+cat_paper %>%
+  left_join(cat, by = "name") %>%
+  unique() -> cat_paper
+
+dates <- seq.Date(from = as.Date( "2004-01-01"), to = as.Date("2021-08-01"), by = "month")
+
+
+as_tibble(gtrends(geo = "DE", time = "all", category = c(47))$interest_over_time)
+
+series <- tibble(date = dates)
+
+missing = NULL
+for (i in cat_paper$id){
+  g <- gtrends(geo = "DE", time = "all", category = i)$interest_over_time
+  if (is.null(g)) missing <- c(missing, cat_paper$name[cat_paper$id == i])
+  else series <- bind_cols(series, i = g$hits)
+}
+
+names(series) <- c("date", setdiff(cat_paper$name, missing))
+
+
+series %>%
+  pivot_longer(cols = -date, names_to = "id", values_to = "hits") -> series
+pc <- ts_pick(ts_prcomp(series), c("PC1", "PC2", "PC3", "PC4", "PC5"))
+
+
+series <- pivot_wider(series, names_from = id, values_from = hits)
+pc <- rename(pivot_wider(pc, names_from = id, values_from = value), date = time)
+
+write.xlsx(left_join(pc, series, by = "date"), file = "Kategorien_und_PC.xlsx")
+
+
+ggplot(series, aes(x = date, y = hits, color = id)) +
+  geom_line() +
+  theme(legend.position = "none")
+
+ggplot(pc, aes(x = time, y = value, color = id)) +
+  geom_line()
