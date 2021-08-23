ddc7774e9257ad531290879617841561f2d58ce1
johannes97s <johannes.schnebel@web.de>
2021-08-23 15:02:39 +0200

    ok

--- R/Index.R
+++ R/Index.R
@@ -12,7 +12,7 @@ pca <- function(keywords = NA,
                 categories = 0,
                 start = as.Date("2004-01-01"),
                 end,
-                n = max(length(kewyords), length(categories))){
+                components = max(length(keywords), length(categories))){
   stopifnot("Nur keywords oder categories darf angegeben werden" = is.na(keywords) | categories == 0)
   dates = seq.Date(as.Date("2004-01-01"), as.Date(end), by = "month")
   dat = tibble(date = NULL, key = NULL, value = NULL)
@@ -31,12 +31,24 @@ pca <- function(keywords = NA,
       dat <- bind_rows(dat, temp)
     }
   }
-  if (roll){
-    }
-  return(pivot_wider(ts_prcomp(dat), names_from = id, values_from = value))
+  return(pivot_wider(ts_prcomp(dat), names_from = id, values_from = value)[1:components])
 }
 
-roll <- function(keywords, categories, start = as.Date("2014-01-01"), end = as.Date("2021-01-01")){
-  period <-  seq.Date(as.Date(start), as.Date(end), by = "month")
+roll <- function(keywords,
+                 categories,
+                 start_series = as.Date("2006-01-01"),
+                 start_period = as.Date("2014-01-01"),
+                 end = as.Date("2021-01-01"),
+                 components = max(length(kewyords), length(categories))){
+  period <-  seq.Date(as.Date(start_period), as.Date(end), by = "month")
+  dates <- seq.Date(as.Date("2006-01-01"), as.Date(end), by = "month")
+  pc <- tibble(date = dates)
+  n <- length(dates)#L?nge der ganzen Reihe
+  for (i in period){
+    d <- as.Date(i, origin = "1970-01-01")
+    pca(keywords = keywords, categories = categories, start = start_series, end = d) %>%
+      select(-time()) -> temp
+    names(temp) <- c()
 
+    }
 }
