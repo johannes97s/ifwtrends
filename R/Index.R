a7090a0754ea3c54d27048fd23c089acdf307f10
johannes97s <johannes.schnebel@web.de>
2021-08-23 11:38:22 +0200

    ok

--- R/Index.R
+++ R/Index.R
@@ -6,18 +6,39 @@ library(openxlsx)
 library(corrr)
 library(lubridate)
 
-g_index <- function(keywords = NULL,
+g_index <- function(keywords = NA,
                     categories = 0,
                     end,
                     start = as.Date("2004-01-01"),
                     roll = F){
+  stopifnot("Nur keywords oder categories darf angegeben werden" = is.na(keywords) | categories == 0)
   dates = seq.Date(as.Date("2004-01-01"), as.Date(end), by = "month")
   if (!roll){
-    ts_gtrends(
-      keyword = kw,
-      geo = "DE",
-      time = "all") %>%
-       filter(time %in% dates) -> dat
-  return(prcomp(ts_ts(dat)))
+    dat = tibble(date = NULL, keyword = NULL, category = NULL, value = NULL)
+    for (kw in keywords){
+      for (cat in categories){
+        as_tibble(gtrends(
+          keyword = kw,
+          category = cat,
+          geo = "DE",
+          time = "all")$interest_over_time)  %>%
+          mutate(date = as.Date(date)) %>%
+          select(date = date, key = any_of(c("keyword", "category")), value = hits) %>%
+          filter(date %in% dates) -> temp
+        dat <- bind_rows(dat, temp)
+      }
+    }
   }
+  return(dat)
 }
+g_index(categories = "651", end = "2021-01-01")
+
+
+dates = seq.Date(as.Date("2004-01-01"), as.Date("2021-01-01"), by = "month")
+t <- gtrends(keyword = c("Wirtschaftskrise"), geo = "DE", time = "all")$interest_over_time
+t %>%
+  mutate(date = as.Date(date)) %>%
+  select(date, keyword, value = hits) %>%
+  filter(date %in% dates) -> tt
+
+prcomp(ts_ts(tt)[-1])
