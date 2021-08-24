17b59ddbc08182f2573e0e3f6c3598878f8d1f6e
johannes97s <johannes.schnebel@web.de>
2021-08-24 17:06:56 +0200

    ok

--- R/Factorloadings.R
+++ R/Factorloadings.R
@@ -5,22 +5,33 @@ library(tsbox)
 library(openxlsx)
 library(forecast)
 
-series <- as_tibble(read.xlsx("~/ifwtrends/Kategorien_und_PC.xlsx", detectDates = T))
-lm(series[[2]] ~ `Events.&.Listings`, data = series)
+dat <- as_tibble(read.xlsx("~/ifwtrends/Kategorien_und_PC.xlsx", detectDates = T))
+s = summary(lm(dat[[3]] ~ dat[[7]], data = dat))
+s$r.squared
 
+
+f <- function(serie, i, dat){
+  summary(lm(dat[[i+1]] ~ serie[[1]]))$r.squared
+}
 
 factorR2 <- function(series, factors){
-  dat <- bind_cols(series, factors)
-  R2 <- tibble(factor = names(factors))
-  mapply(f, )
+  R2 <- tibble()
+  for (i in seq_along(factors)){
+    f <- function(serie) {
+      s <- summary(lm(factors[[i]] ~ serie))
+      s$r.squared
+    }
+    R2 <- bind_rows(R2, apply(series, 2, f))
+  }
+  bind_cols(tibble(factors = str_c("PC",1:length(factors))), R2)
 }
-
-g <- function(i){
-  lm(series[[i+1]], serie, data = series )
-}
-
-f <- function(serie){
-  fac <- 2:6
-  mapply(g, fac)
-}
-
+#
+#
+# factors <- dat[2:6]
+# series <- dat[7:length(dat)]
+#
+#
+# write.xlsx(factorR2(series, factors), "Faktoren_R2.xlsx")
+#
+#
+#
