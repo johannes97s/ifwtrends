bfae4014255e03732f4c8f19dec475b3e164c5c8
johannes97s <johannes.schnebel@web.de>
2021-08-27 12:28:54 +0200

    ok

--- R/other_frequencies.R
+++ R/other_frequencies.R
@@ -17,111 +17,109 @@ library(zoo)
 #
 #
 
+daily_series <- function(keyword = c("Wirtschaftskrise"),
+                         geo = "DE",
+                         from = "2006-01-01"){
+  d <- trendecon:::ts_gtrends_windows(
+    keyword = keyword,
+    geo = geo,
+    from = from,
+    stepsize = "15 days", windowsize = "6 months",
+    n_windows = 348, wait = 20, retry = 10,
+    prevent_window_shrinkage = TRUE
+  )
+  d2 <- trendecon:::ts_gtrends_windows(
+    keyword = keyword,
+    geo = geo,
+    from = seq(Sys.Date(), length.out = 2, by = "-90 days")[2],
+    stepsize = "1 day", windowsize = "3 months",
+    n_windows = 12, wait = 20, retry = 10,
+    prevent_window_shrinkage = FALSE
+  )
+  dd <- trendecon:::aggregate_averages(trendecon:::aggregate_windows(d), trendecon:::aggregate_windows(d2))
 
-keyword = c("Wirtschaftskrise")
-geo = "DE"
+  # download weekly series
+  w <- trendecon:::ts_gtrends_windows(
+    keyword = keyword,
+    geo = geo,
+    from = from,
+    stepsize = "11 weeks", windowsize = "5 years",
+    n_windows = 68, wait = 20, retry = 10,
+    prevent_window_shrinkage = TRUE
+  )
+  w2 <- trendecon:::ts_gtrends_windows(
+    keyword = keyword,
+    geo = geo,
+    from = seq(Sys.Date(), length.out = 2, by = "-1 year")[2],
+    stepsize = "1 week", windowsize = "1 year",
+    n_windows = 12, wait = 20, retry = 10,
+    prevent_window_shrinkage = FALSE
+  )
+  ww <- trendecon:::aggregate_averages(trendecon:::aggregate_windows(w), trendecon:::aggregate_windows(w2))
 
-
-from <- "2006-01-01"
-d <- trendecon:::ts_gtrends_windows(
-  keyword = keyword,
-  geo = geo,
-  from = from,
-  stepsize = "15 days", windowsize = "6 months",
-  n_windows = 200, wait = 20, retry = 10,
-  prevent_window_shrinkage = TRUE
-)
-d2 <- trendecon:::ts_gtrends_windows(
-  keyword = keyword,
-  geo = geo,
-  from = seq(Sys.Date(), length.out = 2, by = "-90 days")[2],
-  stepsize = "1 day", windowsize = "3 months",
-  n_windows = 12, wait = 20, retry = 10,
-  prevent_window_shrinkage = FALSE
-)
-dd <- trendecon:::aggregate_averages(trendecon:::aggregate_windows(d), trendecon:::aggregate_windows(d2))
-
-# download weekly series
-w <- trendecon:::ts_gtrends_windows(
-  keyword = keyword,
-  geo = geo,
-  from = from,
-  stepsize = "11 weeks", windowsize = "5 years",
-  n_windows = 68, wait = 20, retry = 10,
-  prevent_window_shrinkage = TRUE
-)
-w2 <- trendecon:::ts_gtrends_windows(
-  keyword = keyword,
-  geo = geo,
-  from = seq(Sys.Date(), length.out = 2, by = "-1 year")[2],
-  stepsize = "1 week", windowsize = "1 year",
-  n_windows = 12, wait = 20, retry = 10,
-  prevent_window_shrinkage = FALSE
-)
-ww <- trendecon:::aggregate_averages(trendecon:::aggregate_windows(w), trendecon:::aggregate_windows(w2))
-
-# download monthly series
-m <- trendecon:::ts_gtrends_windows(
-  keyword = keyword,
-  geo = geo,
-  from = from,
-  stepsize = "1 month", windowsize = "15 years",
-  n_windows = 1, wait = 20, retry = 10,
-  prevent_window_shrinkage = FALSE
-)
-m2 <- trendecon:::ts_gtrends_windows(
-  keyword = keyword,
-  geo = geo,
-  from = from,
-  stepsize = "1 month", windowsize = "20 years",
-  n_windows = 1, wait = 20, retry = 10,
-  prevent_window_shrinkage = FALSE
-)
-mm <- trendecon:::aggregate_averages(trendecon:::aggregate_windows(m), trendecon:::aggregate_windows(m2))
+  # download monthly series
+  m <- trendecon:::ts_gtrends_windows(
+    keyword = keyword,
+    geo = geo,
+    from = from,
+    stepsize = "1 month", windowsize = "15 years",
+    n_windows = 12, wait = 20, retry = 10,
+    prevent_window_shrinkage = FALSE
+  )
+  m2 <- trendecon:::ts_gtrends_windows(
+    keyword = keyword,
+    geo = geo,
+    from = from,
+    stepsize = "1 month", windowsize = "20 years",
+    n_windows = 12, wait = 20, retry = 10,
+    prevent_window_shrinkage = FALSE
+  )
+  mm <- trendecon:::aggregate_averages(trendecon:::aggregate_windows(m), trendecon:::aggregate_windows(m2))
 
 
 
-dd <- select(dd, -n)
-ww <- select(ww, -n)
-mm <- select(mm, -n)
+  dd <- select(dd, -n)
+  ww <- select(ww, -n)
+  mm <- select(mm, -n)
 
-ww %>%
-  mutate(week = week(time), year = year(time)) %>%
-  filter(week <= 52) %>%
-  select(time, value) -> ww
+  # ww %>%
+  #   mutate(week = week(time), year = year(time)) %>%
+  #   filter(week <= 52) %>%
+  #   select(time, value) -> ww
 
-dd <- ts_regular(ts_dts(dd))
-dd$value <- 0.5*(na.locf(dd$value,fromLast =TRUE) + na.locf(dd$value))
-
-
-ww <-  ts_regular(ts_dts(ww))
-ww$value <- 0.5*(na.locf(ww$value,fromLast =TRUE) + na.locf(ww$value))
-
-mm <-  ts_regular(ts_dts(mm))
-mm$value <- 0.5*(na.locf(mm$value,fromLast =TRUE) + na.locf(mm$value))
-
-# mm %>%
-#   mutate(week = week(time), year = year(time)) %>%
-#   group_by(week, year) %>%
-#   mutate(value = mean(value)) %>%
-#   ungroup() %>%
-#   select( - time) %>%
-#   filter(week <= 52) %>%
-#   unique() %>%
-#   bind_cols(time = ww$time) %>%
-#   select(time, value)-> mm
+  dd <- ts_regular(ts_dts(dd))
+  dd$value <- 0.5*(na.locf(dd$value,fromLast =TRUE) + na.locf(dd$value))
 
 
+  ww <-  ts_regular(ts_dts(ww))
+  ww$value <- 0.5*(na.locf(ww$value,fromLast =TRUE) + na.locf(ww$value))
+
+  mm <-  ts_regular(ts_dts(mm))
+  mm$value <- 0.5*(na.locf(mm$value,fromLast =TRUE) + na.locf(mm$value))
+
+  # mm %>%
+  #   mutate(week = week(time), year = year(time)) %>%
+  #   group_by(week, year) %>%
+  #   mutate(value = mean(value)) %>%
+  #   ungroup() %>%
+  #   select( - time) %>%
+  #   filter(week <= 52) %>%
+  #   unique() %>%
+  #   bind_cols(time = ww$time) %>%
+  #   select(time, value)-> mm
 
 
-wd <- tempdisagg::td(ww ~ dd, method = "fast", conversion = "mean")
-wd <- predict(wd)
 
-mwd <- tempdisagg::td(mm ~ wd, method = "fast", conversion = "mean")
-mwd <- predict(mwd)
+  wd <- tempdisagg::td(ww ~ dd, method = "fast", conversion = "mean")
+  wd <- predict(wd)
+
+  mwd <- tempdisagg::td(mm ~ wd, method = "fast", conversion = "mean")
+  mwd <- predict(mwd)
+  mwd
+}
 
 
-s = 3+3
+
 
 mwd %>%
   mutate(month= floor_date(time, "month")) %>%
