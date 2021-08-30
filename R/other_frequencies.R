584af8748b9d3bf4b17c2a12089c4dbaae367af6
johannes97s <johannes.schnebel@web.de>
2021-08-30 16:46:24 +0200

    Bug at other_frequencies fixed

--- R/other_frequencies.R
+++ R/other_frequencies.R
@@ -15,6 +15,12 @@
 #'@import tidyverse gtrendsR trendecon tsbox lubridate zoo
 #'@export
 
+library(tsbox)
+library(gtrendsR)
+library(trendecon)
+library(tidyverse)
+library(zoo)
+library(lubridate)
 daily_series <- function(keyword = c("arbeitslos"),
                          geo = "DE",
                          from = "2006-01-01"){
@@ -23,7 +29,7 @@ daily_series <- function(keyword = c("arbeitslos"),
     geo = geo,
     from = from,
     stepsize = "15 days", windowsize = "6 months",
-    n_windows = 350, wait = 20, retry = 10,
+    n_windows = 400, wait = 20, retry = 10,
     prevent_window_shrinkage = TRUE
   )
   d2 <- trendecon:::ts_gtrends_windows(
@@ -42,7 +48,7 @@ daily_series <- function(keyword = c("arbeitslos"),
     geo = geo,
     from = from,
     stepsize = "11 weeks", windowsize = "5 years",
-    n_windows = 68, wait = 20, retry = 10,
+    n_windows = 88, wait = 20, retry = 10,
     prevent_window_shrinkage = TRUE
   )
   w2 <- trendecon:::ts_gtrends_windows(
@@ -60,8 +66,8 @@ daily_series <- function(keyword = c("arbeitslos"),
     keyword = keyword,
     geo = geo,
     from = from,
-    stepsize = "1 month", windowsize = "15 years",
-    n_windows = 12, wait = 20, retry = 10,
+    stepsize = "1 month", windowsize = "17 years",
+    n_windows = 1, wait = 20, retry = 10,
     prevent_window_shrinkage = FALSE
   )
   m2 <- trendecon:::ts_gtrends_windows(
@@ -69,7 +75,7 @@ daily_series <- function(keyword = c("arbeitslos"),
     geo = geo,
     from = from,
     stepsize = "1 month", windowsize = "20 years",
-    n_windows = 12, wait = 20, retry = 10,
+    n_windows = 1, wait = 20, retry = 10,
     prevent_window_shrinkage = FALSE
   )
   mm <- trendecon:::aggregate_averages(trendecon:::aggregate_windows(m), trendecon:::aggregate_windows(m2))
@@ -80,20 +86,20 @@ daily_series <- function(keyword = c("arbeitslos"),
   ww <- select(ww, -n)
   mm <- select(mm, -n)
 
-  # ww %>%
-  #   mutate(week = week(time), year = year(time)) %>%
-  #   filter(week <= 52) %>%
-  #   select(time, value) -> ww
-#
-#   dd <- ts_regular(ts_dts(dd))
-#   dd$value <- 0.5*(na.locf(dd$value,fromLast =TRUE) + na.locf(dd$value))
-#
-#
-#   ww <-  ts_regular(ts_dts(ww))
-#   ww$value <- 0.5*(na.locf(ww$value,fromLast =TRUE) + na.locf(ww$value))
-#
-#   mm <-  ts_regular(ts_dts(mm))
-#   mm$value <- 0.5*(na.locf(mm$value,fromLast =TRUE) + na.locf(mm$value))
+  ww %>%
+    mutate(week = lubridate::week(time), year = lubridate::year(time)) %>%
+    filter(week <= 52) %>%
+    select(time, value) -> ww
+
+  dd <- ts_regular(ts_dts(dd))
+  dd$value <- 0.5*(na.locf(dd$value,fromLast =TRUE) + na.locf(dd$value))
+
+
+  ww <-  ts_regular(ts_dts(ww))
+  ww$value <- 0.5*(na.locf(ww$value,fromLast =TRUE) + na.locf(ww$value))
+
+  mm <-  ts_regular(ts_dts(mm))
+  mm$value <- 0.5*(na.locf(mm$value,fromLast =TRUE) + na.locf(mm$value))
 
   # mm %>%
   #   mutate(week = week(time), year = year(time)) %>%
@@ -116,17 +122,24 @@ daily_series <- function(keyword = c("arbeitslos"),
   as_tibble(mwd)
 }
 
-# keyword = "IKEA"
-# geo = "NL"
+###############Tests
+
+
+# keyword = "arbeitslos"
+# geo = "DE"
 # from = "2006-01-01"
 #
-# t %>%
-#   mutate(month= floor_date(time, "month")) %>%
+# series <- daily_series(keyword = "arbeitslos",
+#                        geo = "DE",
+#                        from = "2006-01-01")
+#
+# series %>%
+#   mutate(month = floor_date(time, "month")) %>%
 #   group_by(month) %>%
 #   mutate(monthl_aggr = mean(value)) %>%
 #   ungroup() %>%
 #   select(-month) %>%
-#   left_join(ts_gtrends(keyword = keyword, geo = geo, time = "2006-01-01 2021-08-26", retry = 10), by = "time") -> mwd_mon
+#   left_join(ts_gtrends(keyword = keyword, geo = geo, time = "2006-01-01 2021-08-30", retry = 10), by = "time") -> mwd_mon
 #
 #
 #
@@ -140,6 +153,37 @@ daily_series <- function(keyword = c("arbeitslos"),
 # ggplot(fill(pivot_longer(mwd_mon, cols = -time, names_to = "id", values_to = "value")), aes(x = time, y = value, color = id)) +
 #   geom_line()
 #
-# correlate(mwd_mon$monthl_aggr, mwd_mon$orig)
+# corrr::correlate(mwd_mon$monthl_aggr, mwd_mon$orig)
 #
 # max(abs(mwd_mon$monthl_aggr - mwd_mon$orig))
+#
+#
+#
+#
+#
+#
+#
+# time <- str_c("2006-01-01 ", as.character(Sys.Date()))
+# orig <- select(gtrends(keyword = "arbeitslos",
+#                        geo = "DE",
+#                        time = time)$interest_over_time, date, value = hits)
+# orig %>%
+#   mutate(date = floor_date(as.Date(date), "month")) %>%
+#   group_by(date) %>%
+#   mutate(mnl = mean(value)) %>%
+#   ungroup() %>%
+#   select(date, mnl) %>%
+#   unique() -> orig_avg
+#
+# as_tibble(series) %>% mutate(date = floor_date(as.Date(time), "month")) %>%
+#   group_by(date) %>%
+#   mutate(dail_avg = mean(value)) %>%
+#   ungroup() %>%
+#   select(date, dail_avg) %>%
+#   unique() %>%
+#   left_join(orig_avg, by = "date") %>%
+#   pivot_longer(cols = -date, names_to = "id", values_to = "value") %>%
+#   ggplot(aes(x = date, y = value, color = id)) + geom_line()
+
+
+
