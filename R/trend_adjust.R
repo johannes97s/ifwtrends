b1ee6fda9a7be54e75f35fe40841f216af0ec41a
johannes97s <johannes.schnebel@web.de>
2021-09-03 16:41:19 +0200

    separated adjusting in two functions

--- R/trend_adjust.R
+++ R/trend_adjust.R
@@ -1,12 +1,11 @@
-#'Seasonal and trend adjustment
-#'@description \code{ser_adj} Takes a tibble of timeseries and adds columns with seasonal and trend adjustment
+#'Trend adjustment
+#'@description \code{trend_adj} Takes a tibble of timeseries and returns tibble with trend adjusted values.
 #'
 #'
 #'
-#'@param series The input tibble in tidy form with columns \code{id}, \code{time} and \code{value}. Has to be of monthly frequency.
-#'@param trend Logical, indicates if trend adjustment should be done.
-#'@param seas Logical, indicates if seasonal adjustment should be done.
-#'@param trend_method. Character which method for trend adjustment should be choosen. See Details.
+#'@param series The input tibble in tidy form with columns \code{time}, \code{value} and optional column \code{id}.
+#'@param log.trafo Logical, indicates if value should be transformed to log(value).
+#'@param method. Character which method for trend adjustment should be choosen. See Details.
 #'
 #'For trend_method there can be choosen \code{"firstdiff"} and \code{"comtrend"}.
 #'If \code{"firstdiff"}, first differences with \code{lag = 1} is executed.
@@ -14,35 +13,61 @@
 #'
 #'@example
 #'series <- ts_gtrends(c("ikea", "saturn"), time = "all")
-#'ser_adj(series, trend = T, trend_method = "firstdiff")
+#'trend_adj(series, log.trafo = T, method = "firstdiff")
 #'@import tidyverse gtrendsR tsbox seasonal zoo
 #'@export
-ser_adj <- function(series, trend = T, seas = T, trend_method = "firstdiff", seas_method = "firstdiff"){
-  series <- mutate(series, value = log(value)) #Log-Trafo
-  if(seas && seas_method == "arima"){          #Saisonbereinigung mit X-13 ARIMA
-    if (!("id" %in% names(series))) series <- mutate(series, id = "id")
-    series <- as.list(ts_ts(series))
-    series <- ts_tbl(final(seas(series)))
-  }
-  if(trend && trend_method == "firstdiff"){   #Trendbereinigung mit ersten Differenzen mit lag = 1
+trend_adj <- function(series, log.trafo = F, method = "firstdiff"){
+  if (log.trafo) series <- mutate(series, value = log(value)) #Log-Trafo
+  if(method == "firstdiff"){   #Trendbereinigung mit ersten Differenzen mit lag = 1
     if (!("id" %in% names(series))) series <- mutate(series, id = "id")
     series <- series %>%
       group_by(id) %>%
       mutate(value = c(0, diff(value))) %>%
       ungroup()
   }
-  if(trend && trend_method == "comtrend"){    #Trendbereinigung mit Polynom wie in Paper
+  if(method == "comtrend"){    #Trendbereinigung mit Polynom wie in Paper
                                               #Noch nicht richtig implementier!!
     if (("id" %in% names(series))) fit <- lm(value ~ id -1 +poly(as.numeric(time), 3, raw = T), data = series)
     else fit <- lm(value ~ +poly(as.numeric(time), 3, raw = T), data = series)
     series <- mutate(series, value = fit$residuals)
   }
-  if(seas && seas_method == "firstdiff"){     #Saisonbereinigung mit ersten Differenzen mit lag = 4
-                                              #da gerade Quartalsdaten. Fuer monatsdaten lag = 12
+  series
+}
+
+
+
+#'Seasonal adjustment
+#'@description \code{seas_adj} Takes a tibble of timeseries and returns tibble with seasonal adjusted values.
+#'
+#'@param series The input tibble in tidy form with columns \code{time}, \code{value} and optional column \code{id}. Monthly or quarterly frequency.
+#'@param freq Character "month" or "quarter" for the frequency.
+#'@param log.trafo Logical, indicates if value should be transformed to log(value).
+#'@param method. Character which method for adjustment should be choosen. See Details.
+#'
+#'For method there can be choosen \code{"firstdiff"} and \code{"arima"}.
+#'If \code{"firstdiff"}, first differences with \code{lag = 1} is executed.
+#'If \code{"arima"}, the X-13ARIMA-SEATS of US  procedure is used.
+#'
+#'@example
+#'series <- ts_gtrends(c("ikea", "saturn"), time = "all")
+#'seas_adj(series, freq = "month, log.traf = T, method = "firstdiff")
+#'@import tidyverse gtrendsR tsbox seasonal zoo
+#'@export
+seas_adj <-function(series, freq = "month", log.traf = F, method = "arima"){
+  if (log.trafo) series <- mutate(series, value = log(value)) #Log-Trafo
+  if(method == "arima"){                      #Saisonbereinigung mit X-13 ARIMA
     if (!("id" %in% names(series))) series <- mutate(series, id = "id")
+    series <- as.list(ts_ts(series))
+    series <- ts_tbl(final(seas(series)))
+  }
+  if(method == "firstdiff"){     #Saisonbereinigung mit ersten Differenzen mit lag = 4
+                                 #da gerade Quartalsdaten. Fuer monatsdaten lag = 12
+    if (!("id" %in% names(series))) series <- mutate(series, id = "id")
+    if (freq == "month") k = 12
+    if (freq == "quarter") k = 4
     series <- series %>%
       group_by(id) %>%
-      mutate(value = c(rep(0,4), diff(value, 4))) #Wenn Monatsdaten hier 12 statt 4
+      mutate(value = c(rep(0,k), diff(value, k))) #Wenn Monatsdaten hier 12 statt 4
   }
   series
 }
--- data/keywords_paper.R
+++ data/keywords_paper.R
@@ -1,87 +1,87 @@
-library(tidyverse)
-library(gtrendsR)
-
-
-crisis <- c( "Economic crisis", "Crisis", "Recession", "Financial crisis", 'Krach')
-unemp <- c( "Unemployment", "Unemployment benefits", 'Welfare & Unemployment')
-credit <- c('Student loan', 'Credit & Lending', 'Loan', 'Interest', 'Mortgage', 'Auto Financing')
-consum <- c('Food & Drink', 'GPS & Navigation', 'Performing Arts','Luggage topic', 'Vehicle Brands', 'Birthday',
-            'Travel', 'Energy & Utilities', 'Vehicle Shopping', 'Tobacco Products', 'Health',
-            'Pharmacy', 'Carpooling & Ridesharing', 'Sports', 'Animal Products & Services', 'Fitness', 'Weddings',
-            'Car Rental & Taxi Services', 'Autos & Vehicles', 'Tourist Destinations', 'Home & Garden', 'Events & Listings',
-            'Grocery & Food Retailers', 'Vehicle Licensing & Registration', 'Timeshares & Vacation Properties',
-            'Home Appliances', 'Mass Merchants & Department Stores', 'Car Electronics', 'Fashion & Style', 'Trucks & SUVs',
-            'Home Furnishings', 'Footwear', 'Cruises & Charters', 'Hotels & Accommodations', 'Luggage & Travel Accessories',
-            'Fast Food', 'Book Retailers', 'Veterinarians', 'Spas & Beauty Services', 'Acting & Theater', 'Travel Agencies & Services')
-jobs <- c( 'Waiter', 'Job Listings', 'Resumes & Portfolios', 'Jobs topic', 'Temporary jobs', 'Private employment agency',
-           'Recruitement', 'Developer Jobs', 'Job search')
-bankruptcy <- c('Bankruptcy topic', 'Judicial Liquidation', 'Bankruptcy')
-housing <- c('Affordable housing', 'House price index', 'Apartments & Residential Rentals', 'Home Insurance',
-             'Home Improvement')
-news <- c('Economy News', 'Business News', 'World News', 'Politics', 'Newspapers')
-construction <- c('Flooring', 'Construction Consulting & Contracting', 'Swimming Pools & Spas', 'Civil Engineering', 'Construction & Maintenance')
-pers_fin <- c('Investment','Investing','Financial Planning')
-busin_serv <- c('Data Management', 'Enterprise Technology', 'Accounting & Auditing', 'CAD & CAM', 'Development Tools',
-                'Customer Relationship Management (CRM)', 'Printing & Publishing', 'Corporate Events', 'Computer Security',
-                'Outsourcing', 'Distribution & Logistics', 'Computer Servers', 'Consulting', 'Web Hosting & Domain Registration',
-                'Enterprise Resource Planning (ERP)', 'Business Operations', 'Commercial Vehicles')
-ind_activ <- c('Agriculture & Forestry', 'Agrochemicals', 'Aviation', 'Business & Industrial',
-               'Chemicals Industry', 'Textiles & Nonwovens', 'Coatings & Adhesives', 'Food Production',
-               'Dyes & Pigments', 'Freight & Trucking', 'Transportation & Logistics', 'Mail & Package Delivery',
-               'Manufacturing')
-
-
-topics_paper <- c("Birthday", "Private employment agency" , "House moving", "Unemployment benefits", "Recruitment", "Investment",
-            "Lawyer", "Jobs", "Economic crisis", "Unemployment", "Financial crisis", 'Public debt', "Office space", "Job search",
-            "Temporary jobs", "Housing bubble", "House price index", "Mortgage", "Crisis", "Loan", "Interest", "Student loan",
-            "Affordable housing", "Recession", "Krach", "Bank", "Bankruptcy", "Exportation", "Commercial Building", "Luggage",
-            "Judicial Liquidation", "Foreclosure")
-
-categories_paper <- as_tibble(read.csv("~/ifwtrends/data/Kategorien_Woloszko.csv", header = F))
-names(categories_paper) = "category"
-
-
-cats = as_tibble(categories) %>% select(category = name)
-
-length(crisis) <- 41
-length(unemp) <- 41
-length(credit) <- 41
-length(consum) <- 41
-length(jobs) <- 41
-length(bankruptcy) <- 41
-length(housing) <- 41
-length(news) <- 41
-length(construction) <- 41
-length(pers_fin) <- 41
-length(busin_serv) <- 41
-length(ind_activ) <- 41
-
-
-vars <- tibble(crisis,
-              unemp,
-              credit,
-              consum,
-              jobs,
-              bankruptcy,
-              housing,
-              news,
-              construction,
-              pers_fin,
-              busin_serv,
-              ind_activ) %>%
-  pivot_longer(cols = everything(), names_to = "grouping", values_to = "variable") %>%
-  arrange(grouping) %>%
-  filter(!is.na(variable)) %>%
-  filter(!grepl("topic", variable))
-
-
-
-length(topics_paper) <- 1426
-lookup <- bind_cols(cats, topic = topics_paper) %>%
-  pivot_longer(cols = everything(), names_to = "id", values_to = "variable")
-
-res <- right_join(lookup, vars, by = "variable") %>%
-  select(variable, id, grouping) %>%
-  arrange(grouping)
-
-
+# library(tidyverse)
+# library(gtrendsR)
+#
+#
+# crisis <- c( "Economic crisis", "Crisis", "Recession", "Financial crisis", 'Krach')
+# unemp <- c( "Unemployment", "Unemployment benefits", 'Welfare & Unemployment')
+# credit <- c('Student loan', 'Credit & Lending', 'Loan', 'Interest', 'Mortgage', 'Auto Financing')
+# consum <- c('Food & Drink', 'GPS & Navigation', 'Performing Arts','Luggage topic', 'Vehicle Brands', 'Birthday',
+#             'Travel', 'Energy & Utilities', 'Vehicle Shopping', 'Tobacco Products', 'Health',
+#             'Pharmacy', 'Carpooling & Ridesharing', 'Sports', 'Animal Products & Services', 'Fitness', 'Weddings',
+#             'Car Rental & Taxi Services', 'Autos & Vehicles', 'Tourist Destinations', 'Home & Garden', 'Events & Listings',
+#             'Grocery & Food Retailers', 'Vehicle Licensing & Registration', 'Timeshares & Vacation Properties',
+#             'Home Appliances', 'Mass Merchants & Department Stores', 'Car Electronics', 'Fashion & Style', 'Trucks & SUVs',
+#             'Home Furnishings', 'Footwear', 'Cruises & Charters', 'Hotels & Accommodations', 'Luggage & Travel Accessories',
+#             'Fast Food', 'Book Retailers', 'Veterinarians', 'Spas & Beauty Services', 'Acting & Theater', 'Travel Agencies & Services')
+# jobs <- c( 'Waiter', 'Job Listings', 'Resumes & Portfolios', 'Jobs topic', 'Temporary jobs', 'Private employment agency',
+#            'Recruitement', 'Developer Jobs', 'Job search')
+# bankruptcy <- c('Bankruptcy topic', 'Judicial Liquidation', 'Bankruptcy')
+# housing <- c('Affordable housing', 'House price index', 'Apartments & Residential Rentals', 'Home Insurance',
+#              'Home Improvement')
+# news <- c('Economy News', 'Business News', 'World News', 'Politics', 'Newspapers')
+# construction <- c('Flooring', 'Construction Consulting & Contracting', 'Swimming Pools & Spas', 'Civil Engineering', 'Construction & Maintenance')
+# pers_fin <- c('Investment','Investing','Financial Planning')
+# busin_serv <- c('Data Management', 'Enterprise Technology', 'Accounting & Auditing', 'CAD & CAM', 'Development Tools',
+#                 'Customer Relationship Management (CRM)', 'Printing & Publishing', 'Corporate Events', 'Computer Security',
+#                 'Outsourcing', 'Distribution & Logistics', 'Computer Servers', 'Consulting', 'Web Hosting & Domain Registration',
+#                 'Enterprise Resource Planning (ERP)', 'Business Operations', 'Commercial Vehicles')
+# ind_activ <- c('Agriculture & Forestry', 'Agrochemicals', 'Aviation', 'Business & Industrial',
+#                'Chemicals Industry', 'Textiles & Nonwovens', 'Coatings & Adhesives', 'Food Production',
+#                'Dyes & Pigments', 'Freight & Trucking', 'Transportation & Logistics', 'Mail & Package Delivery',
+#                'Manufacturing')
+#
+#
+# topics_paper <- c("Birthday", "Private employment agency" , "House moving", "Unemployment benefits", "Recruitment", "Investment",
+#             "Lawyer", "Jobs", "Economic crisis", "Unemployment", "Financial crisis", 'Public debt', "Office space", "Job search",
+#             "Temporary jobs", "Housing bubble", "House price index", "Mortgage", "Crisis", "Loan", "Interest", "Student loan",
+#             "Affordable housing", "Recession", "Krach", "Bank", "Bankruptcy", "Exportation", "Commercial Building", "Luggage",
+#             "Judicial Liquidation", "Foreclosure")
+#
+# categories_paper <- as_tibble(read.csv("~/ifwtrends/data/Kategorien_Woloszko.csv", header = F))
+# names(categories_paper) = "category"
+#
+#
+# cats = as_tibble(categories) %>% select(category = name)
+#
+# length(crisis) <- 41
+# length(unemp) <- 41
+# length(credit) <- 41
+# length(consum) <- 41
+# length(jobs) <- 41
+# length(bankruptcy) <- 41
+# length(housing) <- 41
+# length(news) <- 41
+# length(construction) <- 41
+# length(pers_fin) <- 41
+# length(busin_serv) <- 41
+# length(ind_activ) <- 41
+#
+#
+# vars <- tibble(crisis,
+#               unemp,
+#               credit,
+#               consum,
+#               jobs,
+#               bankruptcy,
+#               housing,
+#               news,
+#               construction,
+#               pers_fin,
+#               busin_serv,
+#               ind_activ) %>%
+#   pivot_longer(cols = everything(), names_to = "grouping", values_to = "variable") %>%
+#   arrange(grouping) %>%
+#   filter(!is.na(variable)) %>%
+#   filter(!grepl("topic", variable))
+#
+#
+#
+# length(topics_paper) <- 1426
+# lookup <- bind_cols(cats, topic = topics_paper) %>%
+#   pivot_longer(cols = everything(), names_to = "id", values_to = "variable")
+#
+# res <- right_join(lookup, vars, by = "variable") %>%
+#   select(variable, id, grouping) %>%
+#   arrange(grouping)
+#
+#
