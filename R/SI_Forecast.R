1e63733a14324ddb201d7df29942103dd9a6d0d7
johannes97s <johannes.schnebel@web.de>
2021-09-23 09:52:14 +0200

    all

--- R/SI_Forecast.R
+++ R/SI_Forecast.R
@@ -74,6 +74,8 @@ g_index <- function(
 }
 
 
+start = "2011-01-01"
+end = "2019-12-30"
 
 dat <- readxl::read_xlsx("~/Google Trends/Einzelhandel.xlsx", sheet = "Abbildung") %>%
   select(c(1,3,12))
@@ -84,7 +86,7 @@ names(dat) <- c("time", "einzelhandel", "gastgewerbe")
 dates = seq.Date(as_date("2006-01-01"), as_date("2021-07-01"), by = "month")
 
 dat <- dat %>%
-    select(time, einzelhandel)
+  select(time, einzelhandel)
 
 
 names(dat) <- c("time", "value")
@@ -92,44 +94,51 @@ names(dat) <- c("time", "value")
 dat <- dat %>%
   mutate(value = log(as.numeric(value))) %>%
   mutate(value = c(0, diff(value, 1))) %>%
-  mutate(time = dates)
+  mutate(time = dates) %>%
+  filter(time >= as_date(start), time <= as_date(end))
 
-dat <- dat[-c(1:4),]
+dat <- dat[-c(1, nrow(dat)),]
 
 keyword = c(NA)
-category = c(956, 816,276, 179, 217, 18, 531) #,220,802,1162, 340)
+category_aco= c(956, 276, 179)
+category_ret = c(18, 78, 68, 531, 355, 121, 841)
+category = category_ret
 
+res_raw <- g_index(keyword = keyword, category = category, time = str_c(start, " ", end))
 
+res <- res_raw
 
-res <- g_index(keyword = keyword, category = category, time = str_c("2006-01-01 ", Sys.Date()))
 
 est <- filter(res, time <= max(dat$time))
 
 
 
 
-y <- dat[[2]]
-x <- as.matrix(est[-1])
+y <- dat[2]
+x <- est[-1]
+
+d <- bind_cols(y,x)
+
+
+#cv <- cv.glmnet(x, y, alpha = 0)
+model <- lm(value ~ . , data = d)#glmnet(x, y, alpha = 0, lambda = 0)
 
 
 
-cv <- cv.glmnet(x, y, alpha = 0)
-model <- glmnet(x, y, alpha = 0, lambda = 0)
-coef <- coef(model, s = 0)
-R2 <- model$dev.ratio
 
+x2 <- res[-1]
 
-x2 <- as.matrix(res[-1])
-#dat[nrow(x2)-1,] <- list(max(dat$time) + months(3), NA)
-dat[nrow(x2),] <- list(max(dat$time) + months(3), NA)
-
-
-index<- bind_cols(dat,as_tibble(predict(model, x2, cv$lambda.min)))%>%
-  rename(index = s1)
+dat[nrow(x2)-2,] <- list(max(dat$time) + months(1), NA)
+dat[nrow(x2)-1,] <- list(max(dat$time) + months(1), NA)
+dat[nrow(x2),] <- list(max(dat$time) + months(1), NA)
 
 
 
-openxlsx::write.xlsx(index, "Einzelhandel und Index Niveau.xlsx")
+index<- bind_cols(dat, rename(as_tibble(predict(model, x2)), index = value))%>%
+  left_join(res)
+
+
+openxlsx::write.xlsx(index, "Gastgewerbe und Index.xlsx")
 
 index %>%
   select(time, value, index) %>%
