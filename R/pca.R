ec8bf0a8b712976cc99ab2634c33b3bb415d1a6a
Mathis <brothersunstorm+github@gmail.com>
2021-10-01 16:48:56 +0200

    Fixed a bug where the function wouldnt work on the first day of a month

--- R/pca.R
+++ R/pca.R
@@ -29,30 +29,55 @@
 pca <- function(keywords = NA,
                 categories = 0,
                 geo = "DE",
-                start = "2006-01-01",
+                start = "2006-01-01 CET",
                 end = Sys.Date(),
                 components = max(length(keywords), length(categories))){
   stopifnot("Nur keywords oder categories darf angegeben werden" = is.na(keywords) | categories == 0)
-  dates = seq.Date(as.Date(start), as.Date(end), by = "month")
-  dat = tibble::tibble()
+
+  # Check if function is used on the first day of the month
+  day <- format(end, format = "%d")
+  if (day == "01") {
+    # If indeed it is the first day of the month,
+    # we need to shorten the dates vector by one month because
+    # gtrends data don't include the first day of the month if that's today.
+    end <- seq(end, length = 2, by = "-1 months")[2]
+  }
+
+  dates <-  seq.Date(as.Date(start), as.Date(end), by = "month")
+  dat <-  tibble::tibble()
+
   for (kw in keywords){
     for (cat in categories){
-      tibble::as_tibble(gtrends(
-        keyword = kw,
-        category = cat,
-        geo = geo,
-        time = "all")$interest_over_time) -> temp
-        if (NROW(temp) == 0) stop(str_c("Keine Daten fuer Kategorie ", cat))
-        if ("keyword" %in% names(temp)) temp <- select(temp, -category)
-        temp %>%
-          mutate(date = as_date(date)) %>%
-          select(date, key = any_of(c("keyword", "category")), value = hits) %>%
-          filter(date %in% dates) -> temp
-      dat <- dplyr::bind_rows(dat, temp)
+
+      temp <-
+        tibble::as_tibble(gtrends(
+          keyword = kw,
+          category = cat,
+          geo = geo,
+          time = "all")$interest_over_time
+        )
+
+      if (nrow(temp) == 0) {
+        stop(str_c("Keine Daten fuer Kategorie ", cat))
+      }
+
+      if ("keyword" %in% names(temp)) {
+        temp <- select(temp, -category)
+      }
+
+      temp <- temp %>%
+        mutate(date = as_date(date)) %>%
+        select(date, key = any_of(c("keyword", "category")), value = hits) %>%
+        filter(date %in% dates)
+
+      dat <- bind_rows(dat, temp)
+
     }
   }
-  pc <- bind_cols(date = dates, as_tibble(prcomp(ts_ts(dat))$x))
-  dat <-select(pivot_wider(dat, names_from = key, values_from = value), -date)
 
-  return(bind_cols(pc, dat))
+  pc <- bind_cols(date = dates, as_tibble(prcomp(ts_ts(dat))$x))
+  dat <- select(pivot_wider(dat, names_from = key, values_from = value), -date)
+  result <- bind_cols(pc, dat)
+
+  return(result)
 }
