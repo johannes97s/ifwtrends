2d2d4eab4bf223c5f39cd01abdd5e0619cebaab3
MathisM <brothersunstorm+github@gmail.com>
2021-10-05 12:23:06 +0200

    Create a file containing global vars (Fix #5)

---
+++ R/globals.R
@@ -0,0 +1,11 @@
+# This is just a script to catch the note while check()-ing.
+utils::globalVariables(c(
+  "n",
+  "value",
+  "categories",
+  "id",
+  "category",
+  "hits",
+  "dates",
+  "key"
+))
