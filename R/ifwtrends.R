c5b5452aa38529a4c60d18818c623f836537ab13
MathisM <brothersunstorm+github@gmail.com>
2021-10-05 12:22:14 +0200

    Create a global package help file

---
+++ R/ifwtrends.R
@@ -0,0 +1,20 @@
+#' ifwtrends: A package for evaluating data from Google Trends
+#'
+#' The package ifwtrends is based on trendecon and GtrendsR and takes
+#' data from Google Trends to support economic forecasting.
+#'
+#' @section Functions:
+#' \code{\link{pca}()}
+#'
+#' \code{\link{roll}()}
+#'
+#' \code{\link{daily_series}()}
+#'
+#' \code{\link{factorR2}()}
+#'
+#' For in-depth information and showcases, see the vignette
+#' at \code{vignette(topic = "ifwtrends-demo", package = "ifwtrends")}
+#'
+#' @docType package
+#' @name ifwtrends
+NULL
