45636009304ddc9b14a56e755d363555ffdce80f
johannes97s <johannes.schnebel@web.de>
2021-10-11 13:44:03 +0200

    added forcast eval

--- vignettes/retail.R
+++ vignettes/retail.R
@@ -7,7 +7,7 @@ library(gtrendsR)
 library(tsbox)
 library(RJDemetra)
 
-setwd("~/IFW/ifwtrends")
+setwd("~/ifwtrends")
 start = "2006-01-01"
 end = "2021-07-01"
 
@@ -61,7 +61,7 @@ r1 <-  roll(keyword = NA,
             lags = 2)
 saveRDS(r1, "data/retail_gindex_roll_1219")
 
-start_period_2 = "2019-01-01"
+start_period_2 = "2020-01-01"
 end_2 = "2021-07-31"
 
 r2 <-  roll(keyword = NA,
@@ -75,12 +75,23 @@ r2 <-  roll(keyword = NA,
 saveRDS(r2, "data/retail_gindex_roll_0721")
 
 
+r1 <- readRDS("data/retail_gindex_roll_1219")
+r2 <- readRDS("data/retail_gindex_roll_0721")
 
-r <- readRDS("data/retail_gindex_roll_0721")
+
+r_raw <- c(r1, r2[-c(1:12)])
+#r_raw <- lapply(r_raw, function(x) select(x,time, contains("lag_0")))
+
+r_raw <- lapply(r_raw, function(x) mutate(x, across(.cols = -1, function(x) c(0, diff(x, 1))), .keep = "used"))
+r_raw <- lapply(r_raw, function(x) relocate(bind_cols(x, dat[1:nrow(x),]), time, dat = value))
 
 
-r <- lapply(r, function(x) mutate(x, across(.cols = -1, function(x) c(0, diff(x, 1))), .keep = "used"))
-r <- lapply(r, function(x) relocate(bind_cols(x, dat[1:nrow(x),-1]), time, dat = value))
+r_factors <- lapply(r_raw, function(x){
+  pc <- as_tibble(prcomp(x[-c(1,2)])$x)
+  bind_cols(x[c(1,2)], pc[,1:10])
+  })
+
+r <- r_factors
 
 
 build_model <- function(series){
@@ -89,29 +100,48 @@ build_model <- function(series){
   x <- as.matrix(series[-c(1,2)])
 
 
-  cv <- cv.glmnet(x, y, alpha = 0)
-  model <- glmnet(x, y, alpha = 0, lambda = cv$lambda.min)
+  cv <- cv.glmnet(x, y, alpha = 1)
+  model <- glmnet(x, y, alpha = 1, lambda = 0)#cv$lambda.min)
   model
   # model <- lm(dat ~ ., data = series[-1])
-  # summary(model)$coefficients[,4]
+  # model
+  #summary(model)$coefficients[,4]
 }
 
+covariats <- lapply(r, function(x) as.matrix(x[-c(1,2)]))
 
-models <- lapply(lag(r)[-1], build_model)
+models <- lapply(r, function(x) build_model(x))
 
+pred_values <- mapply(predict, lag(models)[-1], covariats[-1])
+
+last_values <- sapply(pred_values, last)
+
+forec <- tibble(time = seq.Date(as.Date(start_period_1)+months(1), as.Date(end_2), by ="month"),
+                value = last_values) %>%
+  left_join(dat, by = "time") %>%
+  rename(index = value.x, retail =value.y)
+
+sqrt(sum((forec$index - forec$retail)^2))
+
+
+forec %>%
+  pivot_longer(cols = -time, names_to = "id", values_to = "value") %>%
+  ggplot(aes(x=  time, y = value, color = id)) +
+  geom_line()
 
 
 coef <- as_tibble(t(as.data.frame(models))) %>%
-  bind_cols(time = seq.Date(as_date(start_period)+months(1), as_date(end), by = "month"))
+  bind_cols(time = seq.Date(as_date(start_period_1), as_date(end_2), by = "month"))
 
 coef %>%
-  pivot_longer(cols = -time, names_to = "coef", values_to = "pValue") %>%
+  pivot_longer(cols = -time, names_to = "coef", values_to = "value") %>%
   group_by(coef) %>%
-  mutate(mean = mean(pValue)) %>%
-  filter(mean <= 0.4) %>%
-  ggplot(aes(x = time, y = pValue, color = coef)) +
+  mutate(var = var(value)) %>%
+  ungroup() %>%
+  filter(var <= 0.5*mean(var)) %>%
+  ggplot(aes(x = time, y = value, color = coef)) +
   geom_line() +
-  geom_hline(yintercept = 0.1) +
+  theme(legend.position = "none")+
   facet_grid(coef ~ .)
 
 
@@ -130,7 +160,7 @@ forc <- mapply(h, models, r1[-1], SIMPLIFY = F)
 
 
 m1 <- build_model(r1[[1]])
-h(m1, r1[[2]])‚
+h(m1, r1[[2]])
 
 
 
