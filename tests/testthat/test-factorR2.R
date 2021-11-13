14324af5b622e0b5b1fcc5fc15c258f681c04fa0
MathisM <brothersunstorm+github@gmail.com>
2021-11-13 17:13:53 +0100

    Add a skip for the tests when there is no connection to Google

--- tests/testthat/test-factorR2.R
+++ tests/testthat/test-factorR2.R
@@ -1,15 +1,30 @@
+check_connection <- function() {
+  connection_available <- tryCatch(
+    {
+      temp <- gtrendsR::gtrends(keyword = "Ikea", geo = "NL", time = "2021-09-01 2021-10-01")
+      return(TRUE)
+    },
+    error = function(e) {
+      return(FALSE)
+    }
+  )
+}
+
 test_that("function returns a tibble
           respective a list (when a plot is wished)", {
+  skip_if_not(check_connection(), message = "No connection couldn't be established!")
 
-  dat <- pca(keywords = c("Pluto", "Saturn"),
-             categories = 0,
-             geo = "DE",
-             time = paste("2020-01-01", "2020-06-01"))
+  dat <- pca(
+    keywords = c("Pluto", "Saturn"),
+    categories = 0,
+    geo = "DE",
+    time = paste("2020-01-01", "2020-06-01")
+  )
 
 
-  keyword_length <- (dim(dat)[2] - 1)/ 2
-  series <- dat %>% select(date, (2+keyword_length):(1+keyword_length*2))
-  factors <- dat %>% select(date, 2:(1+keyword_length))
+  keyword_length <- (dim(dat)[2] - 1) / 2
+  series <- dat %>% select(date, (2 + keyword_length):(1 + keyword_length * 2))
+  factors <- dat %>% select(date, 2:(1 + keyword_length))
 
   # Without / with plot
   wo_plot_result <- factorR2(series, factors, plot = F)
--- tests/testthat/test-gtpreparation.R
+++ tests/testthat/test-gtpreparation.R
@@ -1,4 +1,18 @@
+check_connection <- function() {
+  connection_available <- tryCatch(
+    {
+      temp <- gtrendsR::gtrends(keyword = "Ikea", geo = "NL", time = "2021-09-01 2021-10-01")
+      return(TRUE)
+    },
+    error = function(e) {
+      return(FALSE)
+    }
+  )
+}
+
 test_that("function returns a tibble", {
+
+  skip_if_not(check_connection(), message = "No connection couldn't be established!")
 
   expect_s3_class(gtpreparation(keyword = c("ikea", "saturn"), time = "2021-01-01 2021-06-01"), "tbl")
 })
