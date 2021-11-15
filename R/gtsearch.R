060f8a8f1b0c50200873311533f8fbb55fd58754
Mathis <brothersunstorm+github@gmail.com>
2021-11-15 16:42:02 +0100

    Now gtsearch can optionally return the data as a tsibble (prior: tibble)

--- R/gtsearch.R
+++ R/gtsearch.R
@@ -8,14 +8,16 @@
 #' returns a time series in form of a tibble with the actual
 #' (in relative terms) search volume of keywords or categories.
 #'
-#' @param keywords A vector (chr) of keywords to search for.
-#' @param categories A vector (numeric) of category numbers to search for.
+#' @param keyword A vector (chr) of keywords to search for.
+#' @param category A vector (numeric) of category numbers to search for.
 #' @param geo The region to search in.
 #' @param timeframe A time frame to search the queries in consisting of
 #' a start date and an end date in YYYY-MM-DD form.
+#' @param as_tbl_ts Logical value to determine if tibble should already
+#' be coerced to a tsibble with keyword or category column as key.
 #'
-#' @return A tibble with a time series of Google Trends search volume from
-#' given inputs.
+#' @return A tsibble with a time series of Google Trends search volume from
+#' given inputs and the keyword respective category as key for it.
 #'
 #' @examples
 #' gtsearch(keywords = c("pluto", "saturn"), timeframe = "2020-01-01 2020-06-01")
@@ -23,27 +25,43 @@
 #' @importFrom dplyr select
 #' @importFrom magrittr %>%
 #' @importFrom gtrendsR gtrends
+#' @importFrom tsibble as_tsibble
+#' @importFrom tibble as_tibble
 #' @export
 gtsearch <- function(keyword = NA,
                      category = NA,
                      geo = "DE",
-                     timeframe = paste("2006-01-01", Sys.Date())) {
+                     timeframe = paste("2006-01-01", Sys.Date()),
+                     as_tbl_ts = TRUE) {
   # stopifnot("You can only enter something either in keyword or in category!" = is.na(keyword) | category == 0)
 
-  if (is.na(keyword)) {
+  if (anyNA(keyword)) {
     result <- gtrends(
       category = category, geo = geo,
       time = timeframe
     )$interest_over_time %>%
       select(date, hits, category) %>%
+      mutate(date = as.Date(date)) %>%
       as_tibble()
-  } else if (is.na(category)) {
+
+    if (as_tbl_ts) {
+      result <- result %>%
+        as_tsibble(key = category)
+    }
+
+  } else if (anyNA(category)) {
     result <- gtrends(
       keyword = keyword, geo = geo,
       time = timeframe
     )$interest_over_time %>%
       select(date, hits, keyword) %>%
+      mutate(date = as.Date(date)) %>%
       as_tibble()
+
+      if (as_tbl_ts) {
+      result <- result %>%
+        as_tsibble(key = keyword)
+    }
   }
 
   return(result)
