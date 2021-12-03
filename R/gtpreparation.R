0bb8f336cc07b45eeece73019217ba010380ff57
MathisM <brothersunstorm+github@gmail.com>
2021-12-03 12:27:28 +0100

    Fix bugs in the id selection

--- R/gtpreparation.R
+++ R/gtpreparation.R
@@ -1,14 +1,58 @@
+#' Helper function to add ID column
+#'
+#' This function checks if the dataframe from Google
+#' contains a column "id". If there is no said column,
+#' this function adds one with the fitting ID from Google.
+#'
+#' @param data A tibble or dataframe containing a Google Trends
+#' time series.
+#' @param keyword The keyword or keywords that have been searched in
+#' the prior function.
+#' @param category The category ID from Google Trends.
+#' @return A tibble with an added ID column, if there was no beforehand.
+#' Otherwise, the original data will be returned.
+#'
+#' @importFrom dplyr mutate
+#' @importFrom dplyr select
+#' @importFrom magrittr %>%
+#' @importFrom tidyselect everything
+#' @keywords internal
+add_id_column <- function(data, keyword, category){
+
+  if (!("id" %in%  colnames(data))) {
+    # Case if a single keyword are used
+    if (length(keyword == 1) & category == 0) {
+      data <- mutate(
+        data, id = keyword
+      ) %>%
+        select(id, everything())
+    } else {
+      # Reformulate the category id into its name
+      data <- mutate(
+        data,
+        id = as.character(
+          gtrendsR::categories[gtrendsR::categories$id == category, 1]
+        )) %>%
+        select(id, everything())
+    }
+    # If more than one keyword is used, the ID
+    # column will be automatically added by the
+    # search function before.
+  }
+  return(data)
+}
+
 #' Preparation of Google Trends data
 #'
 #' @description \code{gtpreparation} downloads for
-#' various search requests respective trends categories
+#' various search requests respective categories
 #' data and applies a logarithmic transformation aswell as a
-#' seasonal adjustment.
+#' seasonal adjustment on downloaded data.
 #' The function returns seasonal adjusted
 #' first derivatives (lagged if desired).
 #'
-#' @param keyword A vector (chr) with search requests.
-#' @param category A category ID from Google Trends. As of now,
+#' @param keyword A character vector with search requests.
+#' @param category A numerical category ID from Google Trends. As of now,
 #' only one category can be given. If you need to use more categories,
 #' the use of a \code{for}-loop is recommended.
 #' @param geo A geographical region to restrict the search queries to.
@@ -18,13 +62,16 @@
 #' As this function will only work on monthly data, you need to enter
 #' for time a time frame that is longer than 5 years.
 #' Otherwise, the time series from Google Trends will be based on
-#' monthly or daily data and cannot be evaluated.
+#' weekly or daily data and cannot be evaluated.
 #' @param lags Number of delays in additional columns (max. value: 4).
+#' Be careful, the first \code{i} months will not be returned
+#' with \code{i} being the number of lags.
 #'
 #' @return Firstly, each row will be log transformed and
-#' seasonal adjusted (via \code{seasonal}'s X-13 ARIMA methods).
+#' seasonal adjusted (via [seasonal::seas()]'s X-13 ARIMA methods).
 #' Furthermore, the first derivatives of these adjusted time
-#' series will be returned (optionally with additional columns containing
+#' series will be returned
+#' (optionally with additional columns containing
 #' lags).
 #'
 #' @import tibble zoo dplyr
@@ -75,30 +122,24 @@ gtpreparation <- function(keyword = NA,
   ) %>%
     mutate(value = log(value)) %>%
     mutate(value = replace(value, value == -Inf, NA_real_)) %>%
-    mutate(value = na.approx(value, rule = 2))
+    mutate(value = na.approx(value, rule = 2)) %>%
+    add_id_column(keyword, category)
 
-
-  # add a id column consisting of the category
-  if (!("id" %in% names(google_data))) {
-    # Reformulate the category id into its name
-    google_data <- mutate(
-      google_data,
-      id = as.character(
-        gtrendsR::categories[gtrendsR::categories$id == category, 1]
-      ))
-  }
 
   # Trend adjust and seasonal adjust data
   adjusted_data <- google_data %>%
     full_join(fit, by = "time") %>%
     mutate(time = as.Date(time), adj = value - trend) %>%
-    select(id, time, adj)   %>%
+    select(id, time, adj) %>%
     seas_adj(method = "arima") %>%
     rename(s_adj = value)
 
+
   # group by category
   grouped_data <- adjusted_data %>%
-    group_by_key()
+    group_by_key() %>%
+    add_id_column(keyword, category)
+
 
   # Add lagged columns
   if (lags >= 1) grouped_data <- mutate(grouped_data, lag_1 = lag(s_adj))
@@ -111,7 +152,7 @@ gtpreparation <- function(keyword = NA,
     ungroup() %>%
     rename(lag_0 = s_adj) %>%
     filter(across(everything(), ~ !is.na(.)))%>%
-    pivot_longer(cols = -c(time), names_to = "lag", values_to = "value") %>%
+    pivot_longer(cols = -c(id, time), names_to = "lag", values_to = "value") %>%
     pivot_wider(names_from = lag, values_from = value)
 
   if (!("id" %in% names(result))) {
